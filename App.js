8f85cb0bb2b1a49dba851773a691d90d1d46f0d4
seirra blake <sophietheopossum@yandex.ru>
2022-02-11 16:51:06 +0000

    fix module path

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import login from 'scrnlogin';
+import login from './scrnlogin';
 const Stack = createNativeStackNavigator();
 export default function App() {
     return (
