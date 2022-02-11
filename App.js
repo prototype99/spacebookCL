602c38efa855c06b3e27465429b6e7f7db8bc668
seirra blake <sophietheopossum@yandex.ru>
2022-02-11 16:17:27 +0000

    fully switch to stack navigation

--- App.js
+++ App.js
@@ -1,41 +1,14 @@
-import React, { Component } from 'react';
-import { Text, TextInput, View, Button, StyleSheet, Alert } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import login from 'scrnlogin';
 const Stack = createNativeStackNavigator();
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      email: '',
-      passwd: ''
-    }
-  }
-  handleEmailInput = (email) => {
-      this.setState({email: email})
-  }
-  handlePasswdInput = (passwd) => {
-      this.setState({passwd: passwd})
-  }
-  login = () => {
-      Alert.alert('spacEmail: ' + this.state.email)
-      Alert.alert('spacepassword: ' + this.state.passwd)
-  }
-  render() {
-    return (
-        <View>
-          <TextInput placeholder="Spaceplease enter your spacEmail" onChangeText={this.handleEmailInput} value={this.state.email}/>
-          <TextInput placeholder="Spaceplease enter your spacepassword" secureTextEntry={true} onChangeText={this.handlePasswdInput} value={this.state.passwd}/>
-          <Button onPress={this.login} title={"Spacelog in"}/>
-        </View>
-    );
-  }
-}
-const styles = StyleSheet.create({
-});
 export default function App() {
-    <NavigationContainer>
-        <Stack.Navigator>
-        </Stack.Navigator>
-    </NavigationContainer>
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Spacelog in" component={login}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
---
+++ scrnlogin.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { TextInput, View, Button, StyleSheet, Alert } from 'react-native';
+class scrnlogin extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      email: '',
+      passwd: ''
+    }
+  }
+  handleEmailInput = (email) => {
+      this.setState({email: email})
+  }
+  handlePasswdInput = (passwd) => {
+      this.setState({passwd: passwd})
+  }
+  login = () => {
+      Alert.alert('spacEmail: ' + this.state.email)
+      Alert.alert('spacepassword: ' + this.state.passwd)
+  }
+  render() {
+    return (
+        <View>
+          <TextInput placeholder="Spaceplease enter your spacEmail" onChangeText={this.handleEmailInput} value={this.state.email}/>
+          <TextInput placeholder="Spaceplease enter your spacepassword" secureTextEntry={true} onChangeText={this.handlePasswdInput} value={this.state.passwd}/>
+          <Button onPress={this.login} title={"Spacelog in"}/>
+        </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+});
+export default scrnlogin
