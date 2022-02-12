396f8a11a7235a84e896b2b9fcc75e6272d0cb25
seirra blake <sophietheopossum@yandex.ru>
2022-02-12 11:07:05 +0000

    oops

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import login from './scrnlogin';
+import scrnlogin from './scrnlogin';
 const Stack = createNativeStackNavigator();
 export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Endless spacepossibilities await!" component={login}/>
+                <Stack.Screen name="Endless spacepossibilities await!" component={scrnlogin}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
