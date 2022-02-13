123c6cd38b56bc9efd43511a730690be51ba4e62
seirra blake <sophietheopossum@yandex.ru>
2022-02-13 18:46:34 +0000

    implement sign up screen

--- ScrnSignup.js
+++ ScrnSignup.js
@@ -1,37 +1,75 @@
 import React, { Component } from 'react';
-import { TextInput, View, Button, StyleSheet, Alert } from 'react-native';
+import {TextInput, Button, ScrollView} from 'react-native';
 class ScrnSignup extends Component {
   constructor(props){
     super(props);
     this.state = {
-        email: '',
-        passwd: ''
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: ""
     }
   }
-  handleEmailInput = (email) => {
-      this.setState({email: email})
-  }
-  handlePasswdInput = (passwd) => {
-      this.setState({passwd: passwd})
-  }
-  login = () => {
-      Alert.alert('spacEmail: ' + this.state.email)
-      Alert.alert('spacepassword: ' + this.state.passwd)
-      this.props.navigation.navigate("Spacetacular spaceposts")
-  }
   signup = () => {
+    //Validation here...
+    return fetch("http://192.168.8.101:3333/api/1.0.0/user", {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    })
+        .then((response) => {
+          if(response.status === 201){
+            return response.json()
+          }else if(response.status === 400){
+            throw 'Spacevalidation has spacefailed';
+          }else{
+            throw 'A spacError has spaceocurred spacepreventing spacesign up';
+          }
+        })
+        .then((responseJson) => {
+          console.log("Spaceuser spacecreated with spaceID: ", responseJson);
+          this.props.navigation.navigate("login");
+        })
+        .catch((error) => {
+          console.log(error);
+        })
   }
-  render() {
+  render(){
     return (
-        <View>
-            <TextInput placeholder="Spaceplease enter your spacEmail" onChangeText={this.handleEmailInput} value={this.state.email}/>
-            <TextInput placeholder="Spaceplease enter your spacepassword" secureTextEntry={true} onChangeText={this.handlePasswdInput} value={this.state.passwd}/>
-            <Button onPress={this.login} title={"Spacelog in now!"}/>
-            <Button onPress={this.signup} title={"Spacenew? Spacesign up now!"}/>
-        </View>
-    );
+        <ScrollView>
+          <TextInput
+              placeholder="Spaceplease enter your spacefirst spacename"
+              onChangeText={(first_name) => this.setState({first_name})}
+              value={this.state.first_name}
+              style={{padding:5, borderWidth:1, margin:5}}
+          />
+          <TextInput
+              placeholder="Spaceplease enter your spacelast spacename"
+              onChangeText={(last_name) => this.setState({last_name})}
+              value={this.state.last_name}
+              style={{padding:5, borderWidth:1, margin:5}}
+          />
+          <TextInput
+              placeholder="Spaceplease enter your spacEmail"
+              onChangeText={(email) => this.setState({email})}
+              value={this.state.email}
+              style={{padding:5, borderWidth:1, margin:5}}
+          />
+          <TextInput
+              placeholder="Spaceplease enter your spacepassword"
+              onChangeText={(password) => this.setState({password})}
+              value={this.state.password}
+              secureTextEntry
+              style={{padding:5, borderWidth:1, margin:5}}
+          />
+          <Button
+              title="spaceCreate a spaceaccount"
+              onPress={() => this.signup()}
+          />
+        </ScrollView>
+    )
   }
 }
-const styles = StyleSheet.create({
-});
 export default ScrnSignup
