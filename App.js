e5db33a7e50a51b926450fe5dd9654c1caf737d0
seirra blake <sophietheopossum@yandex.ru>
2022-02-13 14:14:00 +0000

    improve git compatibility

--- App.js
+++ App.js
@@ -9,9 +9,18 @@ export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Endless spacepossibilities await!" component={scrnlogin}/>
-                <Stack.Screen name="Spacetacular spaceposts" component={scrnpost}/>
-                <Stack.Screen name="Your spacejourney spacebegins!" component={scrnsignup}/>
+                <Stack.Screen
+                    name="Endless spacepossibilities await!"
+                    component={scrnlogin}
+                />
+                <Stack.Screen
+                    name="Spacetacular spaceposts"
+                    component={scrnpost}
+                />
+                <Stack.Screen
+                    name="Your spacejourney spacebegins!"
+                    component={scrnsignup}
+                />
             </Stack.Navigator>
         </NavigationContainer>
     );
