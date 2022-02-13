93808324a58e42139fc62ff75315a0d4ec189b6a
seirra blake <sophietheopossum@yandex.ru>
2022-02-13 17:13:33 +0000

    create basic post screen

--- ScrnPost.js
+++ ScrnPost.js
@@ -1,34 +1,14 @@
 import React, { Component } from 'react';
-import { TextInput, View, Button, StyleSheet, Alert } from 'react-native';
-class ScrnLogin extends Component {
+import { View } from 'react-native';
+class ScrnPost extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      email: '',
-      passwd: ''
-    }
-  }
-  handleEmailInput = (email) => {
-      this.setState({email: email})
-  }
-  handlePasswdInput = (passwd) => {
-      this.setState({passwd: passwd})
-  }
-  login = () => {
-      Alert.alert('spacEmail: ' + this.state.email)
-      Alert.alert('spacepassword: ' + this.state.passwd)
-      this.props.navigation.navigate("scrnpost")
   }
   render() {
     return (
         <View>
-          <TextInput placeholder="Spaceplease enter your spacEmail" onChangeText={this.handleEmailInput} value={this.state.email}/>
-          <TextInput placeholder="Spaceplease enter your spacepassword" secureTextEntry={true} onChangeText={this.handlePasswdInput} value={this.state.passwd}/>
-          <Button onPress={this.login} title={"Spacelog in now!"}/>
         </View>
     );
   }
 }
-const styles = StyleSheet.create({
-});
-export default ScrnLogin
+export default ScrnPost
