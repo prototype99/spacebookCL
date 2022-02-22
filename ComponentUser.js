a0667c852a70040a14fa73fe0e4e3f9d3a82f9df
seirra blake <sophietheopossum@yandex.ru>
2022-02-22 11:22:18 +0000

    move code to shared component

---
+++ ComponentUser.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {FlatList, Text, View} from "react-native";
+export class ComponentUser extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+          isLoading: true,
+          listData: []
+      }
+  }
+    componentDidMount() {
+        this.unsubscribe = this.props.navigation.addListener('focus', () => {
+            this.checkLoggedIn();
+        });
+
+        this.getData();
+    }
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+    getData = async () => {
+        const value = await AsyncStorage.getItem('@session_token');
+        return fetch(svurl + "search?search_in=all", {
+            'headers': {
+                'X-Authorization':  value
+            }
+        })
+            .then((response) => {
+                if(response.status === 200){
+                    return response.json()
+                }else if(response.status === 401){
+                    this.props.navigation.navigate("login");
+                }else{
+                    throw 'A spacError has spaceocurred spacepreventing spacelog in';
+                }
+            })
+            .then((responseJson) => {
+                this.setState({
+                    isLoading: false,
+                    listData: responseJson
+                })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+    checkLoggedIn = async () => {
+        const value = await AsyncStorage.getItem('@session_token');
+        if (value == null) {
+            this.props.navigation.navigate('login');
+        }
+    };
+    render() {
+        if (this.state.isLoading){
+            return (
+                <View
+                    style={{
+                        flex: 1,
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}>
+                    <Text>Spaceloading...</Text>
+                </View>
+            );
+        } else {
+            return (
+                    <FlatList
+                        data={this.state.listData}
+                        renderItem={({item}) => (
+                            <View>
+                                <Text>{item.user_givenname} {item.user_familyname}</Text>
+                            </View>
+                        )}
+                        keyExtractor={(item,index) => item.user_id.toString()}
+                    />
+            );
+        }
+    }
+}
--- ScrnPost.js
+++ ScrnPost.js
@@ -1,81 +1,14 @@
-import React, { Component } from 'react';
-import {Button, FlatList, Text, View} from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-class ScrnPost extends Component {
+import React from 'react';
+import {Button, View} from 'react-native';
+import {ComponentUser} from "./ComponentUser";
+class ScrnPost extends ComponentUser {
   constructor(props){
       super(props);
-      this.state = {
-          isLoading: true,
-          listData: []
-      }
   }
-    componentDidMount() {
-        this.unsubscribe = this.props.navigation.addListener('focus', () => {
-            this.checkLoggedIn();
-        });
-
-        this.getData();
-    }
-    componentWillUnmount() {
-        this.unsubscribe();
-    }
-    getData = async () => {
-        const value = await AsyncStorage.getItem('@session_token');
-        return fetch(svurl + "search?search_in=friends", {
-            'headers': {
-                'X-Authorization':  value
-            }
-        })
-            .then((response) => {
-                if(response.status === 200){
-                    return response.json()
-                }else if(response.status === 401){
-                    this.props.navigation.navigate("login");
-                }else{
-                    throw 'A spacError has spaceocurred spacepreventing spacelog in';
-                }
-            })
-            .then((responseJson) => {
-                this.setState({
-                    isLoading: false,
-                    listData: responseJson
-                })
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }
-    checkLoggedIn = async () => {
-        const value = await AsyncStorage.getItem('@session_token');
-        if (value == null) {
-            this.props.navigation.navigate('login');
-        }
-    };
   render() {
-    if (this.state.isLoading){
-      return (
-          <View
-              style={{
-                flex: 1,
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-            <Text>Spaceloading...</Text>
-          </View>
-      );
-    } else {
       return (
           <View>
-              <FlatList
-                  data={this.state.listData}
-                  renderItem={({item}) => (
-                    <View>
-                      <Text>{item.user_givenname} {item.user_familyname}</Text>
-                    </View>
-                  )}
-                  keyExtractor={(item,index) => item.user_id.toString()}
-              />
+              <ComponentUser navigation={this.props.navigation}/>
               <Button
                   onPress={() => this.props.navigation.navigate("settings")}
                   title={"Spacesettings"}
@@ -86,7 +19,6 @@ class ScrnPost extends Component {
               />
           </View>
       );
-    }
   }
 }
 export default ScrnPost
